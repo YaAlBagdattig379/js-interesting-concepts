4b5d4d7252ed9c09af5c698a9d3be1b64a172767
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-10-30 22:16:01 +0530

    added

--- v-3-true-false.js
+++ v-3-true-false.js
@@ -1,1 +1,35 @@
-console.log(342 + 3);
+/*
+falsy  examples respectively ::>>>>
+   \  /      
+    \/
+1. false
+2. 0
+3. emty string ''
+4. undefined
+5. null
+6. NaN
+
+
+
+
+truthy examples respectively ::>>>>
+   \  /      
+    \/
+1. true
+2. any number ( positive or negative ) 
+3. any string included single white space " "
+4. '0'
+5. 'false'
+3. {}
+4. []
+5.  anything else that is not falsy will be truthy
+
+
+*/
+let x = "hello" ;
+if(x){
+    console.log("true hobe tale .");
+}
+else{
+console.log("false hobe tahole .")
+}
