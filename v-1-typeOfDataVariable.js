4f945126ebb956822c03a0d8b7a795c64ea3e8f1
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-10-30 16:56:11 +0530

    first commit

---
+++ v-1-typeOfDataVariable.js
@@ -0,0 +1,2 @@
+let ltr = 70;
+console.log(typeof ltr); // op >>>   number
---
+++ v-2-primitive.js
@@ -0,0 +1,30 @@
+/*
+      ********  Data types  ********  
+    {   7 types of data in js       }
+
+    1. number
+    2. string
+    3. boolean
+    4. undefined
+    5. null
+    6. object
+    7. symbol
+    
+    
+                 `Data`
+                   /\     
+                  /  \  
+                 /    \  
+                /      \  
+               /        \  
+              /          \  
+      primitive         Non-primitive
+
+    1. number             7. object
+    2. string
+    3. boolean
+    4. undefined
+    5. null
+    6. symbol
+
+*/
