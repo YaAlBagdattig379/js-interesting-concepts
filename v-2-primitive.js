b8a6f45227c32221771ffeed8194aacb6e01fa99
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-10-30 17:31:32 +0530

    added

--- v-2-primitive.js
+++ v-2-primitive.js
@@ -9,8 +9,7 @@
     5. null
     6. object
     7. symbol
-    
-    
+
                  `Data`
                    /\     
                   /  \  
@@ -26,5 +25,33 @@
     4. undefined
     5. null
     6. symbol
+*/
 
+
+// primitive exaples
+let a = 'hello';
+let b = a;
+// console.log(a,b) //op >>>  hello hello 
+a ="gelo";
+// console.log(a,b) //op >>> gelo hello **** emon holo because data rup niache imutable er.
+/*  
+  what is primitive type of data ?
+  >>> jei datar gular single ekta value/man seitai primitive type data 
+       mane bashi jotil noi.
+*/
+
+
+
+
+// Non-primitive exaples
+let x = { job : "web developer"};
+let y = x;
+// console.log(x,y) //op >>>  { job: 'web developer' } { job: 'web developer' }
+x.job="front-end-developer"
+// console.log(x,y)//op>>> { job: 'front-end-developer' } { job: 'front-end-developer' }
+y.job="front-end-developer"
+console.log(y,x)//op>>> { job: 'front-end-developer' } { job: 'front-end-developer' }
+/*  
+what is Non-primitive data ?
+>>>>> etar kono ek jaigai change korle ager pura value/man changed hoa jai but primitive hoina.
 */
