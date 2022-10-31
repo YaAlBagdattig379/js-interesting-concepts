4857f1f0d80081428867d116cd757ca85228d859
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-10-31 17:04:07 +0530

    added

---
+++ v-5-duoble.js
@@ -0,0 +1,38 @@
+/*
+************   double equal and tripe equal   ************
+
+* what is double equal (==) ? and how to use it with exaples ?
+>>> eta sudu dutor man/value normally check kore thake.
+ebong implicit convertion ba coertion kore thake.
+*/
+// exaples :
+const first = 2;
+const second = "2";
+if(first == second){
+    //  console.log('it is true !')
+}
+else{
+    //  console.log('it is worng !')
+} // op >>>>  it is true !
+
+
+/* what is triple equal (===) ? and how to use it with exaples ?
+>>> eta sudu dutor man/value normally check kore thake na borong eta dutor valu
+ebong kon type data  sob gulo jabachai kore thake.
+*/
+// examples 
+const prothom = 2;
+const ditio = "2";
+if(prothom === ditio){
+    console.log("it is true !")
+}
+else{
+    console.log("it is worng !")
+} // op >>> it is worng !   (because eita jacahi bachai kore check kore .)
+
+
+
+ /*
+what is implicit convertion or implicit covertion ?
+>>> bivinno type er variable er data gulo  jei system thik thak kore nei seta kei bole implicit conversion . 
+ */
