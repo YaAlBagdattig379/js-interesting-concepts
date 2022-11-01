a43e9d869e18aac31e30aaee5499aa8789480a7e
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-11-01 09:07:06 +0530

    added

---
+++ v-scope.js
@@ -0,0 +1,8 @@
+// let onion = 200;
+// let potao = 100;
+function shoppping(oni,potao){
+    let results = oni + potao;
+    return results;
+}
+const calc = shoppping(200,100);
+console.log(calc);
