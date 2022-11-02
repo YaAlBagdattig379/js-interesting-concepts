b8fef9d942d68138a05fc2d9761312ef8434c8e7
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-11-02 20:58:25 +0530

    added

---
+++ basic.js
@@ -0,0 +1,16 @@
+/*
+1..fact
+what is an array or not how to know ?
+>>> pls use method -->  Array.isArray(varialbe)
+*/
+const rollNums = [5,53,23,75,23,4];
+// console.log(Array.isArray(rollNums)); // true
+/*
+2..fact
+how to do you know that kon function a koi ta parameter ache ?
+>>> console.log(functionname.length); // 4
+*/
+function hello(ab,af,ag,ah){
+    // console.log(ab,af,ag,ah);
+}
+console.log(hello.length);  // 4
